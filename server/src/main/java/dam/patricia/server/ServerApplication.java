2ba595d4fd4c44fb2fd1ba7ab138a3bc10a919ff
patri <patriigommez@gmail.com>
2026-02-16 18:44:45 +0100

    Dia 2: crear modulos server y client (Spring Boot + SQLite base)

---
+++ server/src/main/java/dam/patricia/server/ServerApplication.java
@@ -0,0 +1,22 @@
+﻿package dam.patricia.server;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Servidor REST en Spring Boot.
+ * Arranca en el puerto 12345 y se conecta a SQLite.
+ */
+@SpringBootApplication
+public class ServerApplication {
+
+    public static void main(String[] args) throws Exception {
+        // Crea la carpeta "data" si no existe (en el directorio desde el que se ejecute)
+        Files.createDirectories(Path.of("data"));
+
+        SpringApplication.run(ServerApplication.class, args);
+    }
+}
